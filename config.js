782d802095dd07dbbf2f4be82b7d540ab26fdf8c
MKDD07 <131703313+MKDD07@users.noreply.github.com>
2026-06-24 10:34:40 +0530

    Update SERP_API_KEY in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // It is ignored by Git. Do NOT commit this file.
 const ENV = {
   PEXELS_API_KEY: 'bPSCecg8osP489H4AQexmZwG3OXpL1DUNjhrX1hafiSE8IapAM9EgZOu',
-  SERP_API_KEY: '8684cad58afd9e73b76ab3d491e063641fa4bead823bbccaa13b11c1399ca915'
+  SERP_API_KEY: 'fada7114fd996c258e2aa198ea0bd4fa1ae9ad269f0cf615a86f72c3b7ce7f91'
 };
